2d349b7fab82036eda8fc3ae87ee8fd00bff64c3
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2019-11-27 15:13:29 +0900

    user.rbの微調整
    validatesメソッドはそれだけでまとめ、定数の宣言などは上にまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
   before_save { self.email = email.downcase }
+  has_secure_password
+
   validates :name,
             presence: true,
             length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email,
             presence: true,
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
-  has_secure_password
   validates :password,
             presence: true,
             length: { minimum: 6 }

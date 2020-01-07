0200616a04c9c97b1f9be13bd8ec79164e1dd36c
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-01-07 11:15:29 +0900

    ユーザー登録失敗時にURLが/signupから/usersに変わる問題を修正

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: {
+      post '/signup', params: {
         user: {
           name: '',
           email: 'user@invalid',
@@ -16,5 +16,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+    assert_select 'form[action="/signup"]'
   end
 end

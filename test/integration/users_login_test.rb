d7336d94f4ff75ac91b79cc500fda639d64cbb89
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-20 14:05:07 +0900

    login成功時のテスト

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,26 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'login_with valid_information' do
+    get login_path
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: 'password'
+      }
+    }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
+  end
+
   test 'login with invalid information' do
     get login_path
     assert_template 'sessions/new'

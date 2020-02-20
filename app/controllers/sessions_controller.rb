cf36049dd559e478d890ec4ea2528d13c387e07b
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-20 11:50:33 +0900

    loginとその後のユーザーページへのリダイレクト実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
-      # Redirect to the user info page agter logging in.
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email / password combination.'
       render 'new'

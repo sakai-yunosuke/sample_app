0bdc6d2c1a31c2e971ad440f645b4b3b1a401c51
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-21 13:35:57 +0900

    remember_meのcheckを入れている時と入れていない時でのテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -44,4 +44,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test 'login without remembering' do
+    # Save cookies then log in
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # Delete cookies then login
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end

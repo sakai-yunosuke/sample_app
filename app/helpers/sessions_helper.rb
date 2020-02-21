67376f2ab41431b4e8a4f3a815d3db5deaf42685
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-21 11:01:58 +0900

    永続cokkiesを作成するメソッドの追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Permanent user session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # Return current log in user
   def current_user
     if session[:user_id]

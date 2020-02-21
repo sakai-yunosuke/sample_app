f1a7507c38b31722016940db7ca6f5ad6dd57c92
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-21 17:44:14 +0900

    編集成功時の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 成功した時の処理を書く
+      flash[:success] = 'Profile updated!'
+      redirect_to @user
     else
       render 'edit'
     end

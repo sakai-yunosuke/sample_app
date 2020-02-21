2cdb6e2fd0621ed5bed264e2bc94e16d3b58af67
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-21 18:12:42 +0900

    ログインしていない状態で編集画面を開くと、ログイン画面にリダイレクトするよう変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -43,5 +45,12 @@ class UsersController < ApplicationController
         :password,
         :password_confirmation
       )
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in'
+        redirect_to login_url
+      end
     end
 end

39c6bfaba7efe13a41b1b1e8f9f9ad68bf3a70ea
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-21 17:24:50 +0900

    users_controllerにupdateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 成功した時の処理を書く
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params

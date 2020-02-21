697f8c250f39eb3a2d697b8da98eb7d590239f44
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-02-21 10:40:12 +0900

    トークン生成用メソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,9 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Return random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 end

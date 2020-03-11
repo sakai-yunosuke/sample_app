d83ac19a44b4e003266b8f047bfb5c7dc1cc4ef3
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-03-11 10:44:50 +0900

    サンプルユーザーとテスト用サンプルユーザーを有効化
    実行コマンド
    $ rails db:migrate:reset
    $ rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@ User.create!(
   email: 'admin@railstutorial.org',
   password: 'foobar',
   password_confirmation: 'foobar',
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 99.times do |n|
@@ -24,5 +26,7 @@ User.create!(
     email: email,
     password: password,
     password_confirmation: password
+    activated: true,
+    activated_at: Time.zone.now
   )
 end

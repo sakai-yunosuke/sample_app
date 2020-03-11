1db0bb210fb0e0ee1df93859c4023adead92dceb
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-03-11 15:50:46 +0900

    authenticated?メソッドの抽象化リファクタ

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,10 +41,10 @@ class User < ApplicationRecord
   end
 
   # Return true if remember_token and digest are the same
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Destroy user's log in info

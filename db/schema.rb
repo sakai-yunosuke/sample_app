169be32b804e1890fd80cced78d4746910e3a5a7
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-03-18 15:01:18 +0900

    migrationに追加した属性の誤字修正
    実行コマンド
    rails db:rollback
    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20200318045224) do
     t.boolean 'activated', default: false
     t.datetime 'activated_at'
     t.string 'reset_digest'
-    t.datetime 'reset_send_at'
+    t.datetime 'reset_sent_at'
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 end

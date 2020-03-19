8188b198e5036ecba027016cc4c44a48fbd50701
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-03-19 14:39:06 +0900

    マイクロポストのcreateアクションを追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,8 +4,21 @@ class MicropostsController < ApplicationController
   before_action :redirect_to_login_unless_logged_in, only: %i[create destroy]
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_page/home'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end

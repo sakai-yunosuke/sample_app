3366997bbfad867fc5d89b086e91f3906f8b58d5
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-03-19 17:16:13 +0900

    画像アップロードのテストを追加

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -11,6 +11,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'ul.pagination'
+    assert_select 'input[type=file]'
     # No content(invalid) post
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: '' } }
@@ -19,8 +20,14 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid post
     content = 'This is valid micropost'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: {
+        micropost: {
+          content: content,
+          picture: picture
+        }
+      }
     end
     assert_redirected_to root_url
     follow_redirect!

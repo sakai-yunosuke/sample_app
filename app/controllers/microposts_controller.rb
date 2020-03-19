827a18829145fc934c67c24180f2f5d919933eb6
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-03-19 16:12:44 +0900

    マイクロポストのdestroyメソッド実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,7 @@
 
 class MicropostsController < ApplicationController
   before_action :redirect_to_login_unless_logged_in, only: %i[create destroy]
+  before_action :redirect_to_root_unless_correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -15,11 +16,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def redirect_to_root_unless_correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end

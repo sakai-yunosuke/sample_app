8c0b2b9067434477239cd57300f8c572ea697446
YunosukeSakai <sakai.yunosuke@moneyforward.co.jp>
2020-03-19 17:21:23 +0900

    画像のフォーマットとサイズに対するバリデーションを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,5 +5,14 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
   mount_uploader :picture, PictureUploader
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, 'should be less than 5MB')
+    end
+  end
 end
